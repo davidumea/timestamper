a571caa53db60c190ea86db522f263d6b29006df
David <david.andersson@elastisys.com>
2024-04-17 16:50:24 +0200

    add image

---
+++ image/script.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+while :
+do
+    # Insert current timestamp into timestamp table
+    psql -h "${PGHOST}" -p 5432 -U "${PGUSER}" -d "${PGDB}" -c "INSERT INTO timestamp (timestamp) (SELECT now()::TIMESTAMP)"
+
+    # Print the 10 latest db inputs
+    echo "$(psql -h "${PGHOST}" -p 5432 -U "${PGUSER}" -d "${PGDB}" -c "SELECT timestamp FROM timestamp ORDER BY id DESC LIMIT 10")"
+
+    sleep 60s
+done
