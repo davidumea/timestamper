0b80b0a8eb4658a75104ec08fcd234da12f66d6a
davidumea <david.andersson@elastisys.com>
2024-12-04 16:08:10 +0100

    require sslmode

--- image/script.sh
+++ image/script.sh
@@ -5,10 +5,10 @@ set -e
 while :
 do
     # Insert current timestamp into timestamp table
-    psql -h "${PGHOST}" -p 5432 -U "${PGUSER}" -d "${PGDB}" -c "INSERT INTO timestamp (timestamp) (SELECT now()::TIMESTAMP)"
+    psql "sslmode=require host=${PGHOST} port=5432 dbname=${PGDB}" -U "${PGUSER}" -c "INSERT INTO timestamp (timestamp) (SELECT now()::TIMESTAMP)"
 
     # Print the 10 latest db inputs
-    echo "$(psql -h "${PGHOST}" -p 5432 -U "${PGUSER}" -d "${PGDB}" -c "SELECT timestamp FROM timestamp ORDER BY id DESC LIMIT 10")"
+    echo "$(psql "sslmode=require host=${PGHOST} port=5432 dbname=${PGDB}" -U "${PGUSER}" -c "SELECT timestamp FROM timestamp ORDER BY id DESC LIMIT 10")"
 
     sleep 60s
 done
